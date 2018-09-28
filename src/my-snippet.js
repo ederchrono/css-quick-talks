ea92b7a9827802eea365ba2044b9f4158d3e3cd6
Eder Diaz <ederchrono@gmail.com>
2018-09-28 11:42:34 -0500

    🎉 initial commit

---
+++ src/my-snippet.js
@@ -0,0 +1,13 @@
+// test
+const sum = 1 + 2;
+const sum1 = 1 + 2;
+const sum2 = 1 + 2;
+const sum3 = 1 + 2;
+const sum4 = 1 + 2;
+const sum5 = 1 + 2;
+const sum6 = 1 + 2;
+const sum11 = 1 + 2;
+const sum22 = 1 + 2;
+const sum33 = 1 + 2;
+const sum44 = 1 + 2;
+const sum55 = 1 + 2;
